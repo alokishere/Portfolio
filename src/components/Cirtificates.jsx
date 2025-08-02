85c14e5638615218310775254d130340a99c64e0
alokishere <alokvishwakarmabdh@gmail.com>
2025-08-03 01:00:33 +0530

    fix: update image paths in Certificates component to remove 'public/' prefix

--- src/components/Cirtificates.jsx
+++ src/components/Cirtificates.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import TiltedCard from "./TiltedCard";
 
 // const certificates = [
-//   'public/image/internship.png',
-//   'public/image/Buildwithindia.png',
-//   'public/image/IIT delhi.jpg',
-//   'public/image/AIIP.jpg',
+//   'image/internship.png',
+//   'image/Buildwithindia.png',
+//   'image/IIT delhi.jpg',
+//   'image/AIIP.jpg',
 // ];
 
 const Certificates = () => {
@@ -14,7 +14,7 @@ const Certificates = () => {
       <h2 className="text-3xl font-bold mb-6 text-center">My Certificates</h2>
       <div className="h-[300px] w-[500px] absolute top-20 left-20">
         <TiltedCard
-          imageSrc="public/image/internship.png"
+          imageSrc="image/internship.png"
           altText="Internship Certificate"
           captionText="Internship Certificate"
           containerHeight="100%"
@@ -31,7 +31,7 @@ const Certificates = () => {
       </div>
       <div className="h-[300px] w-[500px] absolute top-40 right-20">
         <TiltedCard
-          imageSrc="public/image/Buildwithindia.png"
+          imageSrc="image/Buildwithindia.png"
           altText="Build With India"
           captionText="Build With India"
           containerHeight="100%"
@@ -48,7 +48,7 @@ const Certificates = () => {
       </div>
       <div className="h-[300px] w-[500px] absolute bottom-40 left-20">
         <TiltedCard
-          imageSrc="public/image/IIT delhi.jpg"
+          imageSrc="image/IIT delhi.jpg"
           altText="IIT delhi"
           captionText="IIT delhi"
           containerHeight="100%"
@@ -65,7 +65,7 @@ const Certificates = () => {
       </div>
       <div className="h-[300px] w-[500px] absolute bottom-20 right-20">
         <TiltedCard
-          imageSrc="public/image/AIIP.jpg"
+          imageSrc="image/AIIP.jpg"
           altText="AIIP"
           captionText="AIIP"
           containerHeight="100%"
